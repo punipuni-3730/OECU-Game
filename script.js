4637dbb39d1d5422a76e7f1a1bbc4b95d6800682
Sapporo_salmon <57042065+punipuni-3730@users.noreply.github.com>
2024-04-15 16:03:17 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -5,14 +5,19 @@ function start(){
     result.innerHTML="";
     document.getElementById('btn').innerHTML="";
     for (var i = 1; i <= difficult; i++){
-        result.insertAdjacentHTML('beforeend',`<div class="result" id="${i}"><a href="javascript:miss()"><img src="/images/oecu.png"></a><div class="date"><p></p></div><div class="title"><br></div></div>`);
+        result.insertAdjacentHTML('beforeend',`<div class="result" id="${i}"><a href="javascript:miss()"><img src="/OECU-Game/images/oecu.png"></a><div class="date"><p></p></div><div class="title"><br></div></div>`);
         }
-    
+    for (var i = 1; i <= 30; i++){
+    var random = Math.floor(Math.random() * difficult); //0から99
+    let change = document.getElementById( random);
+    change.innerHTML = "";
+    change.insertAdjacentHTML('beforeend',`<a href="javascript:miss()"><img src="/OECU-Game/images/chromium.png"></a><div class="date"><p></p></div><div class="title"><br></div>`);
+    }
     for (var i = 1; i <= 1; i++){
     var random = Math.floor(Math.random() * difficult); //0から99
     let change = document.getElementById( random);
     change.innerHTML = "";
-    change.insertAdjacentHTML('beforeend',`<a href="javascript:clear()" class="chrome"><img src="/images/chrome.png"></a><div class="date"><p></p></div><div class="title"><br></div>`);
+    change.insertAdjacentHTML('beforeend',`<a href="javascript:clear()" class="chrome"><img src="/OECU-Game/images/chrome.png"></a><div class="date"><p></p></div><div class="title"><br></div>`);
     startTime = Date.now();
 }
 }
