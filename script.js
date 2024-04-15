7a2d8842e8630844261b1e60bc8588d26cd6f389
Sapporo_salmon <57042065+punipuni-3730@users.noreply.github.com>
2024-04-16 00:55:24 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,37 @@
 function start(){
     //let difficult = document.getElementById('difficult');
-    let difficult= 100;
+    let difficult= 90;
     let result = document.getElementById('view_links');
     result.innerHTML="";
     document.getElementById('btn').innerHTML="";
     for (var i = 1; i <= difficult; i++){
-        result.insertAdjacentHTML('beforeend',`<div class="result" id="${i}"><a href="javascript:miss()"><img src="/OECU-Game/images/oecu.png"></a><div class="date"><p></p></div><div class="title"><br></div></div>`);
+        result.insertAdjacentHTML('beforeend',`<div class="result" id="${i}"><a href="javascript:miss()" id="a${i}"><img src="/OECU-Game/images/oecu.png" id="img${i}"></div>`);
         }
-    for (var i = 1; i <= 40; i++){
-    var random = Math.floor(Math.random() * difficult); //0から99
-    let change = document.getElementById( random);
-    change.innerHTML = "";
-    change.insertAdjacentHTML('beforeend',`<a href="javascript:miss()"><img src="/OECU-Game/images/chromium.png"></a><div class="date"><p></p></div><div class="title"><br></div>`);
-    }
+
     for (var i = 1; i <= 1; i++){
-    var random = Math.floor(Math.random() * difficult); //0から99
-    let change = document.getElementById( random);
-    change.innerHTML = "";
-    change.insertAdjacentHTML('beforeend',`<a href="javascript:clear()" class="chrome"><img src="/OECU-Game/images/chrome.png"></a><div class="date"><p></p></div><div class="title"><br></div>`);
-    startTime = Date.now();
-    var pass;
-    for (var i = 1; i <= difficult; i++){
-        let id = document.getElementById(i);
-        let classname = id.className;
-        if ("chrome" == classname){
-            pass = pass + 1;
-        }
-        if(pass == 0){
-            start();
-        }
+        var random = Math.floor(Math.random() * difficult); //0から99
+        let img = document.getElementById('img'+random);
+        img.src = '/OECU-Game/images/chromecanary.png'
+        document.getElementById('a'+random).setAttribute('href','javascript:clear()');
+        startTime = Date.now();
     }
-}
+
+    for (var i = 1; i <= 10; i++){
+    var random = Math.floor(Math.random() * difficult); //0から99
+    let img = document.getElementById('img'+random);
+    img.src = '/OECU-Game/images/chromium.png';
+    }
+
+    for (var i = 1; i <= 10; i++){
+        var random = Math.floor(Math.random() * difficult); //0から99
+        let img = document.getElementById('img'+random);
+        img.src = '/OECU-Game/images/chromium.png';
+        }
 }
 
 function miss(){
     alert('失敗です');
     location.reload();
-    
 }
 function clear(){
     clearTime = Date.now() - startTime;
