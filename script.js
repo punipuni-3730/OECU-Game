56a45dcf69aae7acc7fcc6dccfb5613a57daac65
Sapporo_salmon <57042065+punipuni-3730@users.noreply.github.com>
2024-06-14 13:25:50 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
 function start(){
     //let difficult = document.getElementById('difficult');
-    let difficult= 90;
+    let difficult= 120;
     let result = document.getElementById('view_links');
     result.innerHTML="";
     document.getElementById('btn').innerHTML="";
     for (var i = 1; i <= difficult; i++){
-        result.insertAdjacentHTML('beforeend',`<div class="result" id="${i}"><a href="javascript:miss()" id="a${i}"><img src="/OECU-Game/images/oecu.png" id="img${i}"></div>`);
-        }
-
-    for (var i = 1; i <= 1; i++){
-        var random = Math.floor(Math.random() * difficult); //0から99
-        let img = document.getElementById('img'+random);
-        img.src = '/OECU-Game/images/chromecanary.png'
-        document.getElementById('a'+random).setAttribute('href','javascript:clear()');
-        startTime = Date.now();
+        result.insertAdjacentHTML('beforeend',`<div class="result" id="${i}"><a href="javascript:miss()" id="a${i}"><img src="/OECU-Game/images/chromecanary.png" id="img${i}"></a></div>`);
     }
 
-    for (var i = 1; i <= 20; i++){
-    var random = Math.floor(Math.random() * difficult); //0から99
+    var random = Math.floor(Math.random() * difficult); //0から89
     let img = document.getElementById('img'+random);
-    img.src = '/OECU-Game/images/chromium.png';
+    img.src = '/OECU-Game/images/oecu.png';
+    document.getElementById('a'+random).setAttribute('href','javascript:clear()');
+    startTime = Date.now();
+
+    let usedIndexes = new Set([random]);
+    for (var i = 1; i <= 50; i++){
+        do {
+            random = Math.floor(Math.random()*difficult);
+        } while (usedIndexes.has(random));
+        usedIndexes.add(random);
+        img = document.getElementById('img'+random);
+        img.src = '/OECU-Game/images/chromium.png';
+    }
+    for (var i = 1; i <= 50; i++){
+        do {
+            random = Math.floor(Math.random()*difficult);
+        } while (usedIndexes.has(random));
+        usedIndexes.add(random);
+        img = document.getElementById('img'+random);
+        img.src = '/OECU-Game/images/chrome.png';
     }
 }
+
 
 function miss(){
     alert('失敗です');
